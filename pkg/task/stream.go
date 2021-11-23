9eb7c1c2be83714cb49446f6aa840feb362f8280
linuxpizi <1360447275@qq.com>
2021-11-24 01:52:38 +0800

    release grpc client

--- pkg/task/stream.go
+++ pkg/task/stream.go
@@ -2,36 +2,73 @@ package task
 
 import (
 	"context"
-	"os"
+	"time"
 
 	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
 	"github.com/NpoolPlatform/message/npool/signproxy"
+	"github.com/NpoolPlatform/sphinx-plugin/pkg/client"
 	"github.com/NpoolPlatform/sphinx-plugin/pkg/config"
-	"github.com/NpoolPlatform/sphinx-plugin/pkg/conn"
 	"github.com/NpoolPlatform/sphinx-plugin/pkg/plugin/fil"
 	"github.com/NpoolPlatform/sphinx-proxy/pkg/check"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+var (
+	delayDuration = time.Second * 10
+	delay         = make(chan struct{})
+	newConn       = make(chan struct{})
+	startDeal     = make(chan struct{})
+	err           error
+	conn          *grpc.ClientConn
+	proxyClient   signproxy.SignProxy_ProxyPluginClient
 )
 
 func Plugin() {
-	_conn, err := conn.GetGRPCConn(config.GetString(config.KeySphinxProxyAddr))
-	if err != nil {
-		logger.Sugar().Errorf("call GetGRPCConn error: %v", err)
-		os.Exit(1)
-	}
-
-	client := signproxy.NewSignProxyClient(_conn)
-	r, err := client.ProxyPlugin(context.Background())
-	if err != nil {
-		logger.Sugar().Errorf("call ProxyPlugin error: %v", err)
-		os.Exit(1)
-	}
+	go func() {
+		newProxyClinet()
+	}()
 
 	for {
+		select {
+		case <-delay:
+			time.Sleep(delayDuration)
+			newConn <- struct{}{}
+		case <-newConn:
+			newProxyClinet()
+		case <-startDeal:
+			handleTran()
+		}
+	}
+}
+
+func newProxyClinet() {
+	conn, err = client.GetGRPCConn(config.GetString(config.KeySphinxProxyAddr))
+	if err != nil {
+		logger.Sugar().Errorf("call GetGRPCConn error: %v", err)
+		delay <- struct{}{}
+	}
+	pClient := signproxy.NewSignProxyClient(conn)
+	proxyClient, err = pClient.ProxyPlugin(context.Background())
+	if err != nil {
+		logger.Sugar().Errorf("call Transaction error: %v", err)
+		delay <- struct{}{}
+	}
+	startDeal <- struct{}{}
+}
+
+func handleTran() {
+	for {
 		// this will block
-		rep, err := r.Recv()
+		rep, err := proxyClient.Recv()
 		if err != nil {
 			logger.Sugar().Errorf("receiver info error: %v", err)
-			continue
+			if status.Code(err) == codes.Unavailable {
+				proxyClient.CloseSend() // nolint
+				delay <- struct{}{}
+				break
+			}
 		}
 
 		logger.Sugar().Infof(
@@ -55,9 +92,13 @@ func Plugin() {
 		}
 	send:
 		{
-			if err := r.Send(resp); err != nil {
+			if err := proxyClient.Send(resp); err != nil {
 				logger.Sugar().Errorf("send info error: %v", err)
-				continue
+				if status.Code(err) == codes.Unavailable {
+					proxyClient.CloseSend() // nolint
+					delay <- struct{}{}
+					break
+				}
 			}
 		}
 	}
--- pkg/task/task.go
+++ pkg/task/task.go
@@ -2,39 +2,70 @@ package task
 
 import (
 	"context"
-	"os"
+	"time"
 
 	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
 	"github.com/NpoolPlatform/message/npool/signproxy"
 	"github.com/NpoolPlatform/message/npool/sphinxplugin"
+	"github.com/NpoolPlatform/sphinx-plugin/pkg/client"
 	"github.com/NpoolPlatform/sphinx-plugin/pkg/config"
-	"github.com/NpoolPlatform/sphinx-plugin/pkg/conn"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+var (
+	restryDelay = make(chan struct{})
+	next        = make(chan struct{})
+	done        = make(chan struct{})
 )
 
 func RegisterCoin() {
-	_conn, err := conn.GetGRPCConn(config.GetString(config.KeySphinxProxyAddr))
+	go watchRegister()
+	registerCoin()
+}
+
+func watchRegister() {
+lo:
+	for {
+		select {
+		case <-restryDelay:
+			time.Sleep(delayDuration)
+			next <- struct{}{}
+		case <-next:
+			registerCoin()
+		case <-done:
+			break lo
+		}
+	}
+}
+
+func registerCoin() {
+	_conn, err := client.GetGRPCConn(config.GetString(config.KeySphinxProxyAddr))
 	if err != nil {
 		logger.Sugar().Errorf("call GetGRPCConn error: %v", err)
-		os.Exit(1)
+		restryDelay <- struct{}{}
+		return
 	}
 
-	client := signproxy.NewSignProxyClient(_conn)
-	r, err := client.ProxyPlugin(context.Background())
+	sProxyClient := signproxy.NewSignProxyClient(_conn)
+	r, err := sProxyClient.ProxyPlugin(context.Background())
 	if err != nil {
 		logger.Sugar().Errorf("call ProxyPlugin error: %v", err)
-		os.Exit(1)
+		restryDelay <- struct{}{}
+		return
 	}
 
-	for {
-		err := r.Send(&signproxy.ProxyPluginResponse{
-			TransactionType: signproxy.TransactionType_RegisterCoin,
-			CoinType:        sphinxplugin.CoinType_CoinTypeFIL,
-		})
-		if err != nil {
-			logger.Sugar().Errorf("receiver info error: %v", err)
-			continue
+	err = r.Send(&signproxy.ProxyPluginResponse{
+		TransactionType: signproxy.TransactionType_RegisterCoin,
+		CoinType:        sphinxplugin.CoinType_CoinTypeFIL,
+	})
+	if err != nil {
+		logger.Sugar().Errorf("receiver info error: %v", err)
+		if status.Code(err) == codes.Unavailable {
+			r.CloseSend() // nolint
+			restryDelay <- struct{}{}
+			return
 		}
-		r.CloseSend() // nolint
-		break
 	}
+	r.CloseSend() // nolint
 }
