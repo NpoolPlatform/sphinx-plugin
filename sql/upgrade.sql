8a606411a2922c7189ed9c3758094b62897ec116
linuxpizi <1360447275@qq.com>
2022-03-02 16:13:24 +0800

    add support btc upgrade

--- sql/upgrade.sql
+++ sql/upgrade.sql
@@ -1,5 +1,6 @@
 START TRANSACTION;
 
+-- fil
 SELECT
   @pid := `id`,
   @reserved_amount := `reserved_amount`,
@@ -30,6 +31,39 @@ SET
   `for_pay` = @for_pay
 WHERE
   `name` = 'tfilecoin'
+  AND `env` = 'test';
+
+-- btc
+SELECT
+  @pid := `id`,
+  @reserved_amount := `reserved_amount`,
+  @pre_sale := `pre_sale`,
+  @logo := `logo`,
+  @for_pay := `for_pay`
+FROM
+  `sphinx_coininfo`.`coin_infos`
+WHERE
+  `name` = 'bitcoin'
+  AND `env` = 'test';
+
+UPDATE
+  `sphinx_coininfo`.`coin_infos`
+SET
+  `id` = UUID()
+WHERE
+  `name` = 'bitcoin'
+  AND `env` = 'test';
+
+UPDATE
+  `sphinx_coininfo`.`coin_infos`
+SET
+  `id` = @pid,
+  `reserved_amount` = @reserved_amount,
+  `pre_sale` = @pre_sale,
+  `logo` = @logo,
+  `for_pay` = @for_pay
+WHERE
+  `name` = 'tbitcoin'
   AND `env` = 'test';
 
 COMMIT;
