fc079427bd03b77dc2cef1016fc03dfdf1154063
jiangjie <670884108@qq.com>
2022-06-02 19:43:34 +0800

    suport multple-endpoint for eth

--- pkg/plugin/tron/trc20/plugin.go
+++ pkg/plugin/tron/trc20/plugin.go
@@ -2,21 +2,23 @@ package trc20
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"math/big"
 
+	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
 	"github.com/NpoolPlatform/message/npool/sphinxproxy"
 	"github.com/NpoolPlatform/sphinx-plugin/pkg/config"
 	"github.com/NpoolPlatform/sphinx-plugin/pkg/plugin/tron"
 	"github.com/fbsobreira/gotron-sdk/pkg/proto/api"
+	"github.com/fbsobreira/gotron-sdk/pkg/proto/core"
 )
 
 func WalletBalance(ctx context.Context, wallet string) (balance *big.Int, err error) {
 	contract := config.GetENV().Contract
 
-	client, err := tron.Client()
-	if err != nil {
-		return tron.EmptyTRC20, err
-	}
+	client := tron.Client()
+
 	return client.TRC20ContractBalanceS(wallet, contract)
 }
 
@@ -28,9 +30,46 @@ func TransactionSend(ctx context.Context, req *sphinxproxy.ProxyPluginRequest) (
 	amount := req.GetMessage().GetValue()
 	fee := tron.TRC20FeeLimit
 
-	client, err := tron.Client()
+	client := tron.Client()
+
+	return client.TRC20SendS(from, to, contract, ToInt(amount), fee)
+}
+
+func BroadcastTransaction(ctx context.Context, transaction *core.Transaction) (err error) {
+	client := tron.Client()
+
+	result, err := client.BroadcastS(transaction)
 	if err != nil {
-		return nil, err
+		return err
 	}
-	return client.TRC20SendS(from, to, contract, tron.TRC20ToBigInt(amount), fee)
+	if result.Code != 0 {
+		return errors.New(string(result.GetMessage()))
+	}
+	if result.Result {
+		return nil
+	}
+	return errors.New(string(result.GetMessage()))
+}
+
+// done(on chain) => true
+func SyncTxState(ctx context.Context, cid string) (pending bool, exitcode int64, err error) {
+	client := tron.Client()
+
+	txInfo, err := client.GetTransactionInfoByIDS(cid)
+
+	if txInfo == nil || err != nil {
+		return false, 0, ErrWaitMessageOnChain
+	}
+
+	logger.Sugar().Infof("transaction info {CID: %v ,ChainResult: %v, ContractResult: %v, Fee: %v }", cid, txInfo.GetResult(), txInfo.GetReceipt().GetResult(), txInfo.GetFee())
+
+	if txInfo.GetResult() != TransactionInfoSUCCESS {
+		return true, TransactionInfoFAILED, fmt.Errorf("trc20 trasction fail, %v, %v", txInfo.GetResult(), txInfo.GetReceipt().GetResult())
+	}
+
+	if txInfo.Receipt.GetResult() != core.Transaction_Result_SUCCESS {
+		return true, TransactionInfoFAILED, fmt.Errorf("trc20 trasction fail, %v", txInfo.GetReceipt().GetResult())
+	}
+
+	return true, 0, nil
 }
