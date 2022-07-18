bea0e481e11990c48e2bb06af0ce815635a01e71
linuxpizi <1360447275@qq.com>
2022-07-18 21:22:10 +0800

    fix conn

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -3,9 +3,15 @@ package client
 import (
 	"fmt"
 	"strings"
+	"sync"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/connectivity"
+)
+
+var (
+	rlk   sync.RWMutex
+	conns sync.Map
 )
 
 // GetGRPCConn get grpc client conn
@@ -13,18 +19,42 @@ func GetGRPCConn(conn string) (*grpc.ClientConn, error) {
 	if conn == "" {
 		return nil, fmt.Errorf("conn is empty")
 	}
+
 	targets := strings.Split(conn, ",")
 	for _, target := range targets {
-		conn, err := grpc.Dial(target, grpc.WithInsecure())
+		rlk.RLock()
+		_conn, ok := conns.Load(target)
+		if ok {
+			rlk.RUnlock()
+			return _conn.(*grpc.ClientConn), nil
+		}
+		rlk.RUnlock()
+
+		conn, err := getConn(target)
 		if err != nil {
 			continue
 		}
 
-		connState := conn.GetState()
-		if connState != connectivity.Idle && connState != connectivity.Ready {
-			continue
-		}
+		conns.Store(target, conn)
 		return conn, nil
 	}
+
 	return nil, fmt.Errorf("valid conn not found")
+}
+
+func getConn(target string) (*grpc.ClientConn, error) {
+	rlk.Lock()
+	defer rlk.Unlock()
+
+	conn, err := grpc.Dial(target, grpc.WithInsecure())
+	if err != nil {
+		return nil, err
+	}
+
+	connState := conn.GetState()
+	if connState != connectivity.Idle && connState != connectivity.Ready {
+		return nil, err
+	}
+
+	return conn, nil
 }
