c9be9403a693f1d17d09746ae4519a24817ca7ea
linuxpizi <1360447275@qq.com>
2022-07-20 19:12:27 +0800

    fix close invalid grpc conn

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -1,12 +1,14 @@
 package client
 
 import (
+	"context"
 	"fmt"
-	"strings"
 	"sync"
+	"time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/connectivity"
+	"google.golang.org/grpc/health/grpc_health_v1"
 )
 
 var (
@@ -20,26 +22,16 @@ func GetGRPCConn(conn string) (*grpc.ClientConn, error) {
 		return nil, fmt.Errorf("conn is empty")
 	}
 
-	targets := strings.Split(conn, ",")
-	for _, target := range targets {
-		rlk.RLock()
-		_conn, ok := conns.Load(target)
-		if ok {
-			rlk.RUnlock()
-			return _conn.(*grpc.ClientConn), nil
-		}
-		rlk.RUnlock()
-
-		conn, err := getConn(target)
-		if err != nil {
-			continue
-		}
-
-		conns.Store(target, conn)
-		return conn, nil
+	rlk.Lock()
+	_conn, err := checkRemove(conn)
+	rlk.Unlock()
+	if err == nil {
+		return _conn, nil
 	}
 
-	return nil, fmt.Errorf("valid conn not found")
+	// log warn
+
+	return getConn(conn)
 }
 
 func getConn(target string) (*grpc.ClientConn, error) {
@@ -56,5 +48,39 @@ func getConn(target string) (*grpc.ClientConn, error) {
 		return nil, fmt.Errorf("get conn state not ready: %v", connState)
 	}
 
+	conns.Store(target, conn)
 	return conn, nil
+}
+
+// out set ctx
+func checkRemove(target string) (conn *grpc.ClientConn, err error) {
+	_conn, ok := conns.Load(target)
+	if !ok {
+		return nil, fmt.Errorf("server: %v not build conn", target)
+	}
+
+	healthOK := false
+	defer func() {
+		if !healthOK {
+			conn.Close()
+			conns.Delete(target)
+		}
+	}()
+
+	cli := grpc_health_v1.NewHealthClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+
+	hth, err := cli.Check(ctx, &grpc_health_v1.HealthCheckRequest{})
+	if err != nil {
+		return nil, err
+	}
+
+	if hth.GetStatus() != grpc_health_v1.HealthCheckResponse_SERVING {
+		return nil, fmt.Errorf("server: %v is down", target)
+	}
+
+	healthOK = true
+	return _conn.(*grpc.ClientConn), nil
 }
