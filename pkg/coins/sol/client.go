3f6cc926a572432d5c741b299f0dc7dfb1fa1a8e
jiangjie <670884108@qq.com>
2022-07-21 13:25:15 +0800

    add sol-client

---
+++ pkg/coins/sol/client.go
@@ -0,0 +1,74 @@
+package sol
+
+import (
+	"context"
+	"errors"
+	"time"
+
+	"github.com/NpoolPlatform/sphinx-plugin/pkg/endpoints"
+	"github.com/gagliardetto/solana-go/rpc"
+)
+
+const (
+	MinNodeNum       = 1
+	MaxRetries       = 3
+	RetriesSleepTime = 1 * time.Second
+)
+
+type SClientI interface {
+	GetNode(ctx context.Context, endpointmgr *endpoints.Manager) (*rpc.Client, error)
+	WithClient(ctx context.Context, fn func(*rpc.Client) (bool, error)) error
+}
+
+type SClients struct{}
+
+func (sClients SClients) GetNode(ctx context.Context, endpointmgr *endpoints.Manager) (*rpc.Client, error) {
+	endpoint, _, err := endpointmgr.Peek()
+	if err != nil {
+		return nil, err
+	}
+	client := rpc.New(endpoint)
+	_, err = client.GetHealth(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
+
+func (sClients *SClients) WithClient(ctx context.Context, fn func(c *rpc.Client) (bool, error)) error {
+	var (
+		apiErr, err error
+		retry       bool
+	)
+	endpointmgr, err := endpoints.NewManager()
+	if err != nil {
+		return err
+	}
+
+	for i := 0; i < MaxRetries; i++ {
+		if i > 0 {
+			time.Sleep(time.Second)
+		}
+
+		client, err := sClients.GetNode(ctx, endpointmgr)
+		if errors.Is(err, endpoints.ErrEndpointExhausted) {
+			return apiErr
+		}
+
+		if err != nil {
+			return err
+		}
+
+		retry, apiErr = fn(client)
+
+		if !retry {
+			return apiErr
+		}
+	}
+	return err
+}
+
+func Client() SClientI {
+	return &SClients{}
+}
