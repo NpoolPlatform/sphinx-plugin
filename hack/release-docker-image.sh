02802c63384bf4f7f857fa6aa49b653eb19ae044
chen <531759321@qq.com>
2022-09-28 11:50:32 +0000

    update release docker image

--- hack/release-docker-image.sh
+++ hack/release-docker-image.sh
@@ -18,12 +18,23 @@ fi
 
 version=latest
 
-set +e
-version=`git describe --tags --abbrev=0`
-if [ ! $? -eq 0 ]; then
-    version=latest
+## For testing or production environment, pass the second variable
+if [ "x" != "x$2" ]; then
+  version=$2
 fi
-set -e
+
+registry=uhub.service.ucloud.cn
+
+if [ "x" != $3 ]; then
+  registry=$3
+fi
+
+# set +e
+# version=`git describe --tags --abbrev=0`
+# if [ ! $? -eq 0 ]; then
+#     version=latest
+# fi
+# set -e
 
 service_name=$1
 
